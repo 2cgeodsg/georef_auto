51ca020f889139374e6b6e72c8ae779a27a3c8d8
Tiago A Carvalho <ti4gog4mer@gmail.com>
2026-02-18 13:43:12 -0300

    pip verificado também

--- dependencies.py
+++ dependencies.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-def check_dependencies():
+def ensure_dependencies():
     """
     Check and install required dependencies for the plugin.
     """
@@ -10,6 +10,29 @@ def check_dependencies():
         "rasterio": "rasterio",
         "numpy": "numpy"
     }
+    # Garante que o pip tá instalado
+    try:
+        __import__("pip")
+    except:
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip"])
+        except:
+            try:
+                subprocess.check_call([sys.executable, "-m", "ensurepip"])
+                subprocess.check_call([sys.executable, "-m", "pip"])
+            except Exception as e:
+                try:
+                    from qgis.PyQt.QtWidgets import QMessageBox
+                    QMessageBox.critical(
+                        None,
+                        "Error installing dependency",
+                        f"Cannot find or install 'pip'.\n\n"
+                        f"Error: {e}\n\n"
+                        f"Install manually with:\n    python -m ensurepip\n    OR\n    download and run 'get-pip.py'"
+                    )
+                except:
+                    print(f"[Plugin] Cannot find or install 'pip': {e}")
+
     dependencies_all_ok = True
 
     for module, package in packages.items():
@@ -33,7 +56,7 @@ def check_dependencies():
                     print(f"[Plugin] Error installing '{package}': {e}")
     return dependencies_all_ok
 
-if not check_dependencies():
+if not ensure_dependencies():
     raise ImportError("Instalação do plugin falhou.")
 
 import cv2
